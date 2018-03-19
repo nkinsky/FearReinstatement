7b09a45561ce63530978a84f1228e928acc274ca
nkinsky <kinsky@bu.edu>
2018-03-19 11:09:53 -0400

    change dir location

--- session_directory.py
+++ session_directory.py
@@ -8,7 +8,7 @@ Created on Mon Jan 22 09:23:20 2018
 from os import path
 from pickle import load
 
-master_directory = 'U:\Fear conditioning project_Mosaic2\SessionDirectories'
+master_directory = 'E:\Eraser\SessionDirectories'
 
 def load_session_list():
     file = path.join(master_directory, 'SessionDirectories.pkl')
