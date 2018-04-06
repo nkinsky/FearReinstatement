7753a2951c7b481bc198e5cbeae373442490938d
nkinsky <kinsky@bu.edu>
2018-04-06 16:17:10 -0400

    bugfix

--- track_mouse.py
+++ track_mouse.py
@@ -7,6 +7,6 @@ sesh_num -= 1  # adjust to 0 = 1st entry
 t = FFObj(sesh_num)
 print('Tracking mouse')
 t.process_video()
-t.dsave_data()
+t.save_data()
 t.export_pos()
 print('Tracking successfully finished')
