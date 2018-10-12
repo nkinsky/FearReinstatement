97c7feca45320fddea480c1bb37ccc884a5b793d
Nat Kinsky <kinsky@bu.edu>
2018-10-12 07:51:41 -0400

    update to make ScrollPlot work with multiple plot functions

--- plot_functions.py
+++ plot_functions.py
@@ -37,7 +37,7 @@ def overlay_events(obj):
     plot_events(obj)
 
 
-def display_frame(obj):
+def display_frame(obj, ida):
     """
     For plotting FreezeFrame video frames.
     :param obj:
@@ -47,51 +47,55 @@ def display_frame(obj):
     if obj.current_position == 0:
         obj.current_position = round(obj.n_frames / 2)
 
-    obj.ax.imshow(obj.movie[obj.current_position], cmap='gray')
+    obj.ax[ida].imshow(obj.movie[obj.current_position], cmap='gray')
     obj.last_position = obj.n_frames - 1
 
 
-def display_frame_and_position(obj):
+def display_frame_and_position(obj, ida):
     if obj.current_position == 0:
         obj.current_position = round(obj.n_frames / 2)
 
-    obj.ax.imshow(obj.movie[obj.current_position], cmap='gray')
-    obj.ax.plot(obj.position[obj.current_position, 0], obj.position[obj.current_position, 1], 'ro')
+    obj.ax[ida].imshow(obj.movie[obj.current_position], cmap='gray')
+    obj.ax[ida].plot(obj.position[obj.current_position, 0], obj.position[obj.current_position, 1], 'ro')
     obj.last_position = obj.n_frames - 1
 
 
-def display_frame_and_freezing(obj):
+def display_frame_and_freezing(obj, ida):
     if obj.current_position == 0:
         obj.current_position = round(obj.n_frames / 2)
 
     obj.ax.imshow(obj.movie[obj.current_position], cmap='gray')
     if obj.freezing[obj.current_position]:
-        obj.ax.plot(obj.position[obj.current_position, 0], obj.position[obj.current_position, 1], 'bo')
+        obj.ax[ida].plot(obj.position[obj.current_position, 0], obj.position[obj.current_position, 1], 'bo')
     else:
-        obj.ax.plot(obj.position[obj.current_position, 0], obj.position[obj.current_position, 1], 'ro')
+        obj.ax[ida].plot(obj.position[obj.current_position, 0], obj.position[obj.current_position, 1], 'ro')
     obj.last_position = obj.n_frames - 1
 
-def plot_freezing_traces(obj):
-    for i,this_epoch in enumerate(obj.epochs):
+
+def plot_freezing_traces(obj):  # NK ScrollPlot updates have likely broken this
+    for i, this_epoch in enumerate(obj.epochs):
         obj.ax[i].plot(obj.t[this_epoch[0]:this_epoch[1]],
                        obj.traces[obj.current_position, this_epoch[0]:this_epoch[1]])
 
     obj.last_position = len(obj.traces) - 1
 
-def plot_heatmap(obj):
-    obj.ax.imshow(obj.heatmap[obj.current_position])
+
+def plot_heatmap(obj, ida):
+    obj.ax[ida].imshow(obj.heatmap[obj.current_position])
     plt.axis('tight')
 
     obj.last_position = len(obj.heatmap) - 1
 
-def plot_multiple_traces(obj):
-    for i,trace in enumerate(obj.traces[obj.current_position]):
+
+def plot_multiple_traces(obj, ida):
+    for i, trace in enumerate(obj.traces[obj.current_position]):
         c = cm.gray(i/len(obj.traces[obj.current_position]),1)
-        obj.ax.plot(obj.t,trace,color=c)
+        obj.ax[ida].plot(obj.t, trace, color=c)
 
     obj.last_position = len(obj.traces) - 1
 
-def plot_footprints_over_days(obj):
+
+def plot_footprints_over_days(obj): # NK ScrollPlot updates have likely broken this
     for i,footprint in enumerate(obj.footprints[obj.current_position]):
         obj.ax[i].imshow(footprint, cmap='gray')
         obj.ax[i].axis('equal')
@@ -99,14 +103,16 @@ def plot_footprints_over_days(obj):
 
     obj.last_position = len(obj.footprints) - 1
 
-def plot_traces_over_days(obj):
+
+def plot_traces_over_days(obj): # NK ScrollPlot updates have likely broken this
     for i,trace in enumerate(obj.traces[obj.current_position]):
-        obj.ax[i].plot(obj.t[i],trace)
+        obj.ax[i].plot(obj.t[i], trace)
 
     obj.last_position = len(obj.traces) - 1
 
-def plot_raster(obj):
-    obj.ax.eventplot(obj.events[obj.current_position])
-    obj.ax.set_xlim([-obj.window, 0])
+
+def plot_raster(obj, ida):
+    obj.ax[ida].eventplot(obj.events[obj.current_position])
+    obj.ax[ida].set_xlim([-obj.window, 0])
 
     obj.last_position = len(obj.events) - 1
