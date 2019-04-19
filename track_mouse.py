07541075e66a5aed801ffc74bee0aead7a4835fb
nkinsky <kinsky@bu.edu>
2019-04-19 15:03:24 -0400

    random

--- track_mouse.py
+++ track_mouse.py
@@ -12,3 +12,17 @@ t.export_pos()
 print('Tracking successfully finished')
 
 
+
+
+# exporting files to csv files - look in export_pos in ff_video_fixer (shown here below). Will have to change some stuff
+# to write the appropriate variables (e.g. one row could be the day you are calculating and the other row could be the
+# DI value).
+def export_pos(self):
+    import csv
+    directory, _ = path.split(self.avi_location)
+    pos_filename = path.join(directory, 'pos.csv')
+    print(pos_filename)
+    with open(pos_filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(self.position[:, 0])
+        writer.writerow(self.position[:, 1])
